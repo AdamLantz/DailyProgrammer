e42c21fbc1eccbc1a03373a7365a17ecea92d19c
Adam Lantz <adamglantz@gmail.com>
2018-04-21 11:55:29 -0500

    Added alphabet cipher decryption

--- alphabet-cipher/alphabet-cipher.js
+++ alphabet-cipher/alphabet-cipher.js
@@ -1,9 +1,17 @@
 (function () {
 
-    let inputs = ['snitch thepackagehasbeendelivered',
+    let inputs = [
+        'snitch thepackagehasbeendelivered',
         'bond theredfoxtrotsquietlyatmidnight',
         'train murderontheorientexpress',
-        'garden themolessnuckintothegardenlastnight'];
+        'garden themolessnuckintothegardenlastnight'
+    ];
+
+    let bonusInputs = [
+        'cloak klatrgafedvtssdwywcyty',
+        'python pjphmfamhrcaifxifvvfmzwqtmyswst',
+        'moore rcfpsgfspiecbcc'
+    ];
 
     inputs.forEach(input => {
         let inputValues = input.split(' ');
@@ -11,6 +19,14 @@
         let message = inputValues[1];
         console.log(encryptMessage(secret, message));
     });
+
+    bonusInputs.forEach(input => {
+        let inputValues = input.split(' ');
+        let secret = inputValues[0];
+        let message = inputValues[1];
+        console.log(decryptMessage(secret, message));
+    });
+
 
     function getValueFromLetter(letter) {
         return letter.charCodeAt(0) - 97;
@@ -31,6 +47,19 @@
 
     function encryptLetter(secretLetter, messageLetter) {
         return getLetterFromValue((getValueFromLetter(secretLetter) + getValueFromLetter(messageLetter)) % 26);
+    }
+
+    function decryptMessage(secret, encrypted) {
+        let message = '';
+        let encryptedChars = encrypted.split('');
+        for (let i = 0; i < encryptedChars.length; i++) {
+            message += decryptLetter(secret.charAt(i % secret.length), encryptedChars[i]);
+        }
+        return message;
+    }
+
+    function decryptLetter(secretLetter, encryptedLetter) {
+        return getLetterFromValue((getValueFromLetter(encryptedLetter) - getValueFromLetter(secretLetter) + 26) % 26);
     }
 
 })();
